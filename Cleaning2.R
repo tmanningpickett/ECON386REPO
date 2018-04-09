770b49c90271aee8d4c166aa0b6634cc8beb079f
tmanningpickett <35975710+tmanningpickett@users.noreply.github.com>
2018-04-09 14:37:41 -0700

    Add files via upload

---
+++ Cleaning2.R
@@ -0,0 +1,12 @@
+## TMP: I used read.table function on the .txt file, which returned an error message that the number
+## of variables in row 1 were not equal to those in subsequent rows. I imported the data in an Excel
+## file and saved it as a .csv, removing the first two rows to eliminate the issue.
+
+## TMP: Imported .csv into "Panel_8595.2" file in R.
+Panel_8595.2 <- read.table("C:/Users/Trin/Desktop/Panel 8595.csv")
+
+## TMP: Created column names based on those given in the .txt file (Y1:F2) and filled in headers for
+## the last three columns (C1:C2). 
+colnames(Panel_8595.2) <- c("Y1", "Y2", "Y3", "X1", "X2", "X3", "X4", 
+                            "X5", "F1", "F2", "C1", "C2", "C3")
+View(Panel_8595.2)
